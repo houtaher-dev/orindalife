3ccf70e9754bbbc52d11285ac33c9b75db83800a
houtaher-dev <h.outaher@gmail.com>
2026-05-13 05:23:57 +0200

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,12 +2,14 @@ import { Hero } from "@/components/Hero";
 import { ProductsSection } from "@/components/ProductsSection";
 import { FeaturesSection } from "@/components/FeaturesSection";
 import { HowItWorksSection } from "@/components/HowItWorksSection";
+import { ReviewsSection } from "@/components/ReviewsSection";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <ProductsSection />
+      <ReviewsSection />
       <FeaturesSection />
       <HowItWorksSection />
     </>
